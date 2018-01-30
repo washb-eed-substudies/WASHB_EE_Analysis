44c5910b4d785a40782f17a3650d3532f2462a97
“amertens” <amertens@berkeley.edu>
2018-01-30 13:27:47 -0800

    Updated seasonality figure

--- R EE outcome code/EE-BD-seasonality-figure.R
+++ R EE outcome code/EE-BD-seasonality-figure.R
@@ -6,6 +6,7 @@
 
 
 rm(list=ls())
+library(plyr)
 library(RColorBrewer)
 library(scales)
 library(foreign)
@@ -48,29 +49,50 @@ df<-rbind(stool, urine)
 #Set calendar date variable
 df$caldate <- as.Date(df$date,format="%d%b%Y")
 
+#create month and year variables
+df$month <-  as.numeric(month(df$caldate))
+df$year <- as.numeric(year(df$caldate))
+df$study_month <- df$month + (df$year - min(df$year, na.rm=T))*12
+
 #log transform outcome
 df$logvalue <- log(df$value)
 
+#drop empty tr factor levels
+df <- droplevels(df)
+
+df <- df %>% group_by(variable) %>%
+    mutate(logvalue=scale(logvalue))
 
 #Set colors:
 tableau10 <- c("#1F77B4","#FF7F0E","#2CA02C","#D62728",
                "#9467BD","#8C564B","#E377C2","#7F7F7F","#BCBD22","#17BECF")
 
 p1<- ggplot(df, aes(x = caldate)) +
-    #geom_point(aes(x=caldate, y=logvalue, group=tr, color=tr),  alpha = 0.1, size=0.5) +
     geom_smooth(aes(y=logvalue, group=tr, color=tr)) +
-      facet_wrap(~variable, scale="free_y") +
+    #geom_rug(aes(y=logvalue, group=tr, color=tr),sides="b",alpha = 1/2, position = "jitter") +
+    geom_segment(data=df[df$tr=="Control",], aes(x=caldate,y=-2,xend =caldate, yend = -1.75), color=tableau10[1], alpha=0.1) +
+    geom_segment(data=df[df$tr=="WSH",], aes(x=caldate,y=-1.75,xend =caldate, yend = -1.5), color=tableau10[2], alpha=0.1) +
+    geom_segment(data=df[df$tr=="Nutrition",], aes(x=caldate,y=-1.5,xend =caldate, yend = -1.25), color=tableau10[3], alpha=0.1) +
+    geom_segment(data=df[df$tr=="Nutrition + WSH",], aes(x=caldate,y=-1.25,xend =caldate, yend = -1), color=tableau10[4], alpha=0.1) +
+      facet_wrap(~variable, ncol=4) +
     scale_colour_manual( values=tableau10[1:4]) +
     labs(x = "Date",
-         y = "ln-value",
-         title = "Ln biomarker concentration over calendar time",
+         y = "z-scored ln-value",
+         title = "Z-scored Ln biomarker conc. over calendar time with lines marking measurements",
          color="Intervention Arm") +
     theme(#panel.border = element_blank(), 
           strip.background = element_blank() )#,
           #legend.position="none")
 
+
+#Add density plots to margins
+ggplot(df, aes(x = caldate)) +
+  geom_density(aes(group=tr, color=tr, fill=tr), alpha=0.2) +
+  facet_wrap(~variable, scale="free_y") +
+  scale_colour_manual( values=tableau10[1:4])
+
 p2<- ggplot(df, aes(x = caldate)) +
-    geom_point(aes(x=caldate, y=logvalue, group=tr, color=tr),  alpha = 0.1, size=0.5) +
+    geom_point(aes(x=caldate, y=logvalue, group=tr, color=tr),  alpha = 0.1, size=2) +
     geom_smooth(aes(y=logvalue, group=tr, color=tr)) +
       facet_wrap(~variable, scale="free_y") +
     scale_colour_manual( values=tableau10[1:4]) +
@@ -85,3 +107,168 @@ p2<- ggplot(df, aes(x = caldate)) +
 setwd("C:/Users/andre/Dropbox/WASHB-EE-analysis/WBB-EE-analysis/Results/Figures")
 ggsave(filename="EED-biomarker-seasonality-smooth.pdf",plot = p1,device='pdf',width=9,height=9)
 ggsave(filename="EED-biomarker-seasonality-smoothpoints.pdf",plot = p2,device='pdf',width=9,height=9)
+
+
+#median studymonth br round and arm
+df %>% group_by(time,tr) %>% summarize(mean=mean(study_month, na.rm=T), median=median(study_month, na.rm=T))
+
+
+#Histogram of campling times
+ggplot(df, aes(x=study_month, fill=tr)) +
+    facet_wrap(~variable) +
+    geom_histogram( position="dodge")
+
+ggplot(df, aes(x=study_month, fill=tr)) +
+    facet_wrap(~variable) +
+    geom_histogram( alpha=.5, position="identity")
+
+#Arm sampling proportion by day (should switch to week or month)
+plot_df <- ddply(df,.(study_month, variable),summarise,
+              percentage=as.numeric(prop.table(table(tr))),
+              tr=names(table(tr)))
+ggplot(plot_df) + geom_bar(aes(y = percentage, x = study_month, fill = tr),stat="identity") + facet_wrap(~variable) 
+
+
+
+
+
+
+p3<- ggplot(df, aes(x = caldate)) +
+    geom_smooth(aes(y=logvalue, group=tr, color=tr)) +
+    #geom_rug(aes(y=logvalue, group=tr, color=tr),sides="b",alpha = 1/2, position = "jitter", sixe=12) +
+    #coord_cartesian(ylim = c()) +
+      facet_wrap(~variable, scale="free_y", ncol = 7) +
+    scale_colour_manual( values=tableau10[1:4]) +
+    labs(x = "Date",
+         y = "ln-value",
+         title = "Ln biomarker concentration over calendar time",
+         color="Intervention Arm") +
+    theme(#panel.border = element_blank(), 
+          strip.background = element_blank(),
+          legend.position="none")
+
+
+#Add density plots to margins
+p4<-ggplot(df, aes(x = caldate)) +
+  geom_density(aes(group=tr, color=tr, fill=tr), alpha=0.2) +
+  facet_wrap(~variable, scale="free_y", ncol = 7) +
+  scale_colour_manual( values=tableau10[1:4]) +
+    theme(#panel.border = element_blank(), 
+          strip.background = element_blank(),
+          legend.position="none")
+
+
+
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+
+  numPlots = length(plots)
+
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                    ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+
+ if (numPlots==1) {
+    print(plots[[1]])
+
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
+multiplot(p3,p4, cols=1)
+
+
+df1 <- df %>% filter(time==1 & variable!="reg1b")
+df2 <- df %>% filter(time==2)
+df3 <- df %>% filter(time==3 & variable!="reg1b")
+
+setwd("C:/Users/andre/Dropbox/WASHB-EE-analysis/WBB-EE-analysis/Results/Figures/")
+
+pdf("EED-seasonality-plots.pdf",width=9,height=9)
+pR1<- ggplot(df1, aes(x = caldate)) +
+    geom_smooth(aes(y=logvalue, group=tr, color=tr)) +
+    #geom_rug(aes(y=logvalue, group=tr, color=tr),sides="b",alpha = 1/2, position = "jitter") +
+    geom_segment(data=df1[df1$tr=="Control",], aes(x=caldate,y=-2,xend =caldate, yend = -1.75), color=tableau10[1], alpha=0.25) +
+    geom_segment(data=df1[df1$tr=="WSH",], aes(x=caldate,y=-1.75,xend =caldate, yend = -1.5), color=tableau10[2], alpha=0.25) +
+    geom_segment(data=df1[df1$tr=="Nutrition",], aes(x=caldate,y=-1.5,xend =caldate, yend = -1.25), color=tableau10[3], alpha=0.25) +
+    geom_segment(data=df1[df1$tr=="Nutrition + WSH",], aes(x=caldate,y=-1.25,xend =caldate, yend = -1), color=tableau10[4], alpha=0.25) +
+      facet_wrap(~variable, ncol=3) +
+    scale_colour_manual( values=tableau10[1:4]) +
+    labs(x = "Date",
+         y = "z-scored ln-value",
+         title = "Z-scored Ln biomarker conc. over calendar time - round 1",
+         color="Intervention Arm") +
+    theme(#panel.border = element_blank(), 
+          strip.background = element_blank() )#,
+          #legend.position="none")
+pR1
+
+pR2<- ggplot(df2, aes(x = caldate)) +
+    geom_smooth(aes(y=logvalue, group=tr, color=tr)) +
+    #geom_rug(aes(y=logvalue, group=tr, color=tr),sides="b",alpha = 1/2, position = "jitter") +
+    geom_segment(data=df2[df2$tr=="Control",], aes(x=caldate,y=-2,xend =caldate, yend = -1.75), color=tableau10[1], alpha=0.25) +
+    geom_segment(data=df2[df2$tr=="WSH",], aes(x=caldate,y=-1.75,xend =caldate, yend = -1.5), color=tableau10[2], alpha=0.25) +
+    geom_segment(data=df2[df2$tr=="Nutrition",], aes(x=caldate,y=-1.5,xend =caldate, yend = -1.25), color=tableau10[3], alpha=0.25) +
+    geom_segment(data=df2[df2$tr=="Nutrition + WSH",], aes(x=caldate,y=-1.25,xend =caldate, yend = -1), color=tableau10[4], alpha=0.25) +
+      facet_wrap(~variable, ncol=4) +
+    scale_colour_manual( values=tableau10[1:4]) +
+    labs(x = "Date",
+         y = "z-scored ln-value",
+         title = "Z-scored Ln biomarker conc. over calendar time - round 2",
+         color="Intervention Arm") +
+    theme(#panel.border = element_blank(), 
+          strip.background = element_blank() )#,
+          #legend.position="none")
+pR2
+
+pR3<- ggplot(df3, aes(x = caldate)) +
+    geom_smooth(aes(y=logvalue, group=tr, color=tr)) +
+    #geom_rug(aes(y=logvalue, group=tr, color=tr),sides="b",alpha = 1/2, position = "jitter") +
+    geom_segment(data=df3[df3$tr=="Control",], aes(x=caldate,y=-2,xend =caldate, yend = -1.75), color=tableau10[1], alpha=0.25) +
+    geom_segment(data=df3[df3$tr=="WSH",], aes(x=caldate,y=-1.75,xend =caldate, yend = -1.5), color=tableau10[2], alpha=0.25) +
+    geom_segment(data=df3[df3$tr=="Nutrition",], aes(x=caldate,y=-1.5,xend =caldate, yend = -1.25), color=tableau10[3], alpha=0.25) +
+    geom_segment(data=df3[df3$tr=="Nutrition + WSH",], aes(x=caldate,y=-1.25,xend =caldate, yend = -1), color=tableau10[4], alpha=0.25) +
+      facet_wrap(~variable, ncol=3) +
+    scale_colour_manual( values=tableau10[1:4]) +
+    labs(x = "Date",
+         y = "z-scored ln-value",
+         title = "Z-scored Ln biomarker conc. over calendar time - round 3",
+         color="Intervention Arm") +
+    theme(#panel.border = element_blank(), 
+          strip.background = element_blank() )#,
+          #legend.position="none")
+pR3
+dev.off()
+
+
+
+
+
+
+
+
+
+#Plot stratified by time and by arm. Maybe add an actual mean of observations, 
+#and an expected mean if the observations had been balanced by arm
+
+
